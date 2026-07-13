a0c6e3aa1021de22dfd4d58a67771a14d118c8e7
Harsha4576dks <harshivahm@gmail.com>
2026-07-13 15:53:20 +0530

    hospital_database

--- hospital.py
+++ hospital.py
@@ -6,8 +6,9 @@ app = FastAPI()
 
 records = {
     1:{
-        "name":"Dr. vinod",
+        "name":"Dr.vinod",
         "specialization":"cardiologist",
+        "experience":5,
         "consultation_fees":500
     }
 }
@@ -15,7 +16,14 @@ records = {
 class Doctor(BaseModel):
     name:str
     specialization:str
+    experience:int
     consultation_fees:int
+
+class updateDoctor(BaseModel):
+    name:str|None=None
+    specialization:str|None=None
+    experience:int|None=None
+    consultation_fees:int|None=None
 
 @app.post("/records/{Doctor_id}",status_code = status.HTTP_201_CREATED)
 def create_Doctor(Doctor_id:int, doctor:Doctor):
@@ -36,3 +44,16 @@ def search_by_specialization(specialization: str):
         raise HTTPException(status_code=404,detail="No doctors found with this specialization")
 
     return matched_doctors
+
+@app.put("/records/{Doctor_id}")
+def update_Doctor(Doctor_id:int, Doctor:updateDoctor):
+   if Doctor_id not in records:
+        raise HTTPException(status_code = 400, detail = "This doctor is not available in the list") 
+
+   for key, value in Doctor.model_dump(exclude_unset = True).items():
+    if value is not None:
+        records[Doctor_id][key] = value
+            
+    return {"message":"This doctors data is updated successsfully", "updated_Doctor":records[Doctor_id]}
+   
+   
